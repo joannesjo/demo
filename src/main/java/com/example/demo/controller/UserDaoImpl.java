ec36cdb4590231d1ca3c29186476c86e40201e31
joannes <joannes.webd@gmail.com>
2019-03-24 19:21:25 +0000

    Initial commit

---
+++ src/main/java/com/example/demo/controller/UserDaoImpl.java
@@ -0,0 +1,44 @@
+package com.example.demo.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+
+@Repository
+public class UserDaoImpl {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    public JasperPrint exportPdfFile() throws SQLException, JRException, IOException {
+        Connection conn = jdbcTemplate.getDataSource().getConnection();
+
+        String path = resourceLoader.getResource("classpath:rpt_users.jrxml").getURI().getPath();
+
+        JasperReport jasperReport = JasperCompileManager.compileReport(path);
+
+        // Parameters for report
+        Map<String, Object> parameters = new HashMap<String, Object>();
+
+        JasperPrint print = JasperFillManager.fillReport(jasperReport, parameters, conn);
+
+        return print;
+    }
+}
